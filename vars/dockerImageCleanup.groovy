4a6cab490dab8b8297ff83f0b65afeba15c61378
Vaishali-Parande <vaishali.l.parande@gmail.com>
2025-01-10 16:04:38 +0530

    cloning shared lib at local

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -10,4 +10,5 @@ def call(String project, String ImageTag, String hubUser){
 //     sh """
 //      docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
 //     """
-// }
+// 
+//}
